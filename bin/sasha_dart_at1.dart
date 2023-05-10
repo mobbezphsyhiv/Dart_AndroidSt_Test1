3c2f416894ef2d970aeaf5ec3948fe3bad0bbc40
mobbezphsyhiv <127360324+mobbezphsyhiv@users.noreply.github.com>
2023-05-10 13:20:02 +0300

    first commit

---
+++ bin/sasha_dart_at1.dart
@@ -0,0 +1,5 @@
+import 'package:sasha_dart_at1/sasha_dart_at1.dart' as sasha_dart_at1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sasha_dart_at1.calculate()}!');
+}
---
+++ test/sasha_dart_at1_test.dart
@@ -0,0 +1,8 @@
+import 'package:sasha_dart_at1/sasha_dart_at1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
