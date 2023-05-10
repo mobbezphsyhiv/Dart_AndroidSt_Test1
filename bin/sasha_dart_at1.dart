72425609814421414a4d899c535364d2667dca54
mobbezphsyhiv <127360324+mobbezphsyhiv@users.noreply.github.com>
2023-05-10 13:28:09 +0300

    changed

--- bin/sasha_dart_at1.dart
+++ bin/sasha_dart_at1.dart
@@ -1,5 +1,5 @@
 import 'package:sasha_dart_at1/sasha_dart_at1.dart' as sasha_dart_at1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${sasha_dart_at1.calculate()}!');
+  print('Goodbye world: ${sasha_dart_at1.calculate()}!');
 }
